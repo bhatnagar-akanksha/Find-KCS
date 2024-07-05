c9a78b2808b547924a1862b6dab80f97fc7ecde4
bhatnagar-akanksha <101624537+bhatnagar-akanksha@users.noreply.github.com>
2024-07-05 18:45:28 +0530

    core application structure

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var test=[
         'createdBy':'Akanksha Bhatnagar',
         'createdOn':'21-06-2024',
         'tags':['feeds','warehouse'],
-        'ticketID':'E-12343444'
+        'CJA':'E-12343444'
     },
         {
             'name':"KCS TITLE8",
@@ -13,7 +13,7 @@ var test=[
             'createdBy':'Shruti Pradhan',
             'createdOn':'12-06-2024',
             'tags':['test-feeds','warehouse'],
-            'ticketID':'E-1234555',
+            'CJA':'E-1234555',
         },
         {
             'name':"KCS TITLE2",
@@ -21,7 +21,7 @@ var test=[
             'createdBy':'Akanksha Bhatnagar',
             'createdOn':'12-06-2024',
             'tags':['test-feeds','warehouse'],
-            'ticketID':'E-1256788',
+            'CJA':'E-1256788',
         },
         {
             'name':"KCS TITLE0",
@@ -29,7 +29,7 @@ var test=[
             'createdBy':'Rishita',
             'createdOn':'12-06-2024',
             'tags':['test-feeds','warehouse'],
-            'ticketID':'E-123497',
+            'CJA':'E-123497',
         },
         {
             'name':"KCS TITLE4",
@@ -37,7 +37,7 @@ var test=[
             'createdBy':'Faraz',
             'createdOn':'12-06-2024',
             'tags':['test-feeds','warehouse'],
-            'ticketID':'E-123546567',
+            'CJA':'E-123546567',
         },
         {
             'name':"KCS TITLE5",
@@ -45,7 +45,7 @@ var test=[
             'createdBy':'Nilotpal',
             'createdOn':'12-06-2024',
             'tags':['calcudated matrics','warehouse'],
-            'ticketID':'E-12345557',
+            'CJA':'E-12345557',
         },
         {
             'name':"KCS TITLE6",
@@ -53,13 +53,112 @@ var test=[
             'createdBy':'Akanksha Bhatnagar',
             'createdOn':'12-06-2024',
             'tags':['segment','warehouse'],
-            'ticketID':'E-123455455',
+            'CJA':'E-123455455',
         }
 
 
-
-
 ]
+let Sheet1=[
+ 
+    {
+     "createdOn": "04\/03\/2024",
+     "publisherName": "Nishita Badola",
+     "Subject": "Clicks to page dimension in new data warehouse UI",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=04926a9d-13cf-ee11-9078-000d3a3110f0"
+    },
+    {
+     "createdOn": "04\/03\/2024",
+     "publisherName": "Chirag Kulshreshtha",
+     "Subject": "Populate Mobile Lat\/Long type map in Analytics using AEP Mobile SDK",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=e86d4a32-35da-ee11-904d-6045bd006704"
+    },
+    {
+     "createdOn": "04\/03\/2024",
+     "publisherName": "Faraz Husain",
+     "Subject": "When a video has no ad then why mediaStarts is bigger than contentStarts?",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=26a41ea9-3fda-ee11-904d-6045bd006ce9"
+    },
+    {
+     "createdOn": "3\/21\/2024",
+     "publisherName": "Faraz Husain",
+     "Subject": "Demdex calls blocked in Iframe due ICP limitation in We Chat Mini Program",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=97fc0310-61e7-ee11-904d-6045bd006268"
+    },
+    {
+     "createdOn": "5\/13\/2024",
+     "publisherName": "Prabhjeet Bhatia",
+     "Subject": "[CJA] Reporting differences between CJA and AJO",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=d08de63d-5916-ef11-9f8a-6045bd02b206",
+     "CJA": "Y"
+    },
+    {
+     "createdOn": "5\/13\/2024",
+     "publisherName": "Nilotpal Bharti",
+     "Subject": "Configuring Azure Blob for Classification Ingestion in Adobe Analytics [Classification Sets]",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=c9de3f40-fa10-ef11-9f8a-6045bd006268 "
+    },
+    {
+     "createdOn": "5\/14\/2024",
+     "publisherName": "Shruti Pradhan",
+     "Subject": "How to pass multiple Pixel IDs in Event forwarding?",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=89813912-0812-ef11-9f89-000d3a372703"
+    },
+    {
+     "createdOn": "5\/20\/2024",
+     "publisherName": "Prabhjeet Bhatia",
+     "Subject": "[AA] How to fix ACDL error window[t].getState is not a function while actioning 'Reset Data Layer'",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=ad70f61f-5e16-ef11-9f8a-6045bd02b206"
+    },
+    {
+     "createdOn": "5\/20\/2024",
+     "publisherName": "Prabhjeet Bhatia",
+     "Subject": "[AA] Do updates of component descriptions on Data Dictionary get communicated back to the Analytics 2.0 API?",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=8bd4f5a3-6016-ef11-9f8a-6045bd02b206"
+    },
+    {
+     "createdOn": "5\/24\/2024",
+     "publisherName": "Nilotpal Bharti",
+     "Subject": "How can logs be tracked for projects that were unshared from the Company folder?",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=6aaaab72-801b-ef11-840b-6045bd034c54 "
+    },
+    {
+     "createdOn": "5\/24\/2024",
+     "publisherName": "Nilotpal Bharti",
+     "Subject": "How to resolve garbled values in reporting?",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=22a6bd19-841b-ef11-840b-6045bd034c54 "
+    },
+    {
+     "createdOn": "5\/24\/2024",
+     "publisherName": "Nilotpal Bharti",
+     "Subject": "Move to' option not available for Admins",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=d364d7a3-871b-ef11-840b-6045bd034c54"
+    },
+    {
+     "createdOn": "5\/24\/2024",
+     "publisherName": "Nilotpal Bharti",
+     "Subject": "How to block Analytics cookies from being set without ECID extension?",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=a5b42afb-8a1b-ef11-840b-6045bd034c54"
+    },
+    {
+     "createdOn": "             5\/31\/2024",
+     "publisherName": "Mohammad Faizan",
+     "Subject": "Daily data feeds not receiving Adobe Advertising Cloud data sources.",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=f2e74f34-7119-ea11-a811-000d3a5936c5&forceUCI=1&pagetype=entityrecord&etn=knowledgearticle&id=d641645e-bf1e-ef11-840b-6045bd006704"
+    },
+    {
+     "createdOn": "    5\/31\/2024",
+     "publisherName": "Mohammad Faizan",
+     "Subject": "Fallout discrepancy  Visits vs Visitor fallout report showing different numbers for same touchpoints",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=8d3e9ad6-c11e-ef11-840b-6045bd006704"
+    },
+    {
+     "createdOn": "             5\/31\/2024",
+     "publisherName": "Mohammad Faizan",
+     "Subject": "List vars not getting tracked in server calls.",
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=6a189a64-6c1f-ef11-840a-002248092444"
+    }
+   ]
+   
     
 function format(d) {
     // `d` is the original data object for the row
@@ -86,8 +185,9 @@ let table = new DataTable('#myTable', {
      {data: ''},
     { data: 'createdOn' },
     { data: 'createdBy' },  
-    { data: 'ticketID' },
-    { data: 'description', visible:false }
+    { data: 'CJA' },
+    { data: 'description', visible:false },
+    { data: ''}
    
  
 ],
@@ -105,6 +205,14 @@ columnDefs: [
             return `<p class='tags'>${row.tags[0]}</p>`;
         }
     },
+    {
+        targets: 7,
+        render: function (data, type, row, meta) {
+
+            return `<p class='tags'>delete</p>`;
+        }
+    },
+
     {
             "defaultContent": "-",
             "targets": "_all"
@@ -128,3 +236,12 @@ table.on('click', 'td.dt-control', function (e) {
         row.child(format(row.data())).show();
     }
 });
+
+
+$('#toggleToolbar').on('click', function(){
+    if ($('#secondary-toolbar').is(':visible')) {
+        $('#secondary-toolbar').hide()
+      }else{
+        $('#secondary-toolbar').show()
+      }
+})
