a86f1e8aaef02d8659fc9289d53472cfaa6aabac
bhatnagar-akanksha <101624537+bhatnagar-akanksha@users.noreply.github.com>
2024-07-29 22:00:46 +0530

    filter functioning and UI improvements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 var test=[
         {
         'name':"KCS TITLE1",
@@ -64,178 +65,283 @@ let Sheet1=[
      "createdOn": "04\/03\/2024",
      "publisherName": "Nishita Badola",
      "Subject": "Clicks to page dimension in new data warehouse UI",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=04926a9d-13cf-ee11-9078-000d3a3110f0"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=04926a9d-13cf-ee11-9078-000d3a3110f0",
+     "tag":[]
     },
     {
      "createdOn": "04\/03\/2024",
      "publisherName": "Chirag Kulshreshtha",
      "Subject": "Populate Mobile Lat\/Long type map in Analytics using AEP Mobile SDK",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=e86d4a32-35da-ee11-904d-6045bd006704"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=e86d4a32-35da-ee11-904d-6045bd006704",
+     "tag":[]
     },
     {
      "createdOn": "04\/03\/2024",
      "publisherName": "Faraz Husain",
      "Subject": "When a video has no ad then why mediaStarts is bigger than contentStarts?",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=26a41ea9-3fda-ee11-904d-6045bd006ce9"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=26a41ea9-3fda-ee11-904d-6045bd006ce9",
+     "tag":[]
     },
     {
      "createdOn": "3\/21\/2024",
      "publisherName": "Faraz Husain",
      "Subject": "Demdex calls blocked in Iframe due ICP limitation in We Chat Mini Program",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=97fc0310-61e7-ee11-904d-6045bd006268"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=97fc0310-61e7-ee11-904d-6045bd006268",
+     "tag":[]
     },
     {
      "createdOn": "5\/13\/2024",
      "publisherName": "Prabhjeet Bhatia",
      "Subject": "[CJA] Reporting differences between CJA and AJO",
      "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=d08de63d-5916-ef11-9f8a-6045bd02b206",
-     "CJA": "Y"
+     "CJA": "Y",
+     "tag":['CJA','AJO']
     },
     {
      "createdOn": "5\/13\/2024",
      "publisherName": "Nilotpal Bharti",
      "Subject": "Configuring Azure Blob for Classification Ingestion in Adobe Analytics [Classification Sets]",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=c9de3f40-fa10-ef11-9f8a-6045bd006268 "
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=c9de3f40-fa10-ef11-9f8a-6045bd006268 ",
+     "tag":['classification']
     },
     {
      "createdOn": "5\/14\/2024",
      "publisherName": "Shruti Pradhan",
      "Subject": "How to pass multiple Pixel IDs in Event forwarding?",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=89813912-0812-ef11-9f89-000d3a372703"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=89813912-0812-ef11-9f89-000d3a372703",
+     "tag":[]
     },
     {
      "createdOn": "5\/20\/2024",
      "publisherName": "Prabhjeet Bhatia",
      "Subject": "[AA] How to fix ACDL error window[t].getState is not a function while actioning 'Reset Data Layer'",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=ad70f61f-5e16-ef11-9f8a-6045bd02b206"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=ad70f61f-5e16-ef11-9f8a-6045bd02b206",
+     "tag":['ACDL']
     },
     {
      "createdOn": "5\/20\/2024",
      "publisherName": "Prabhjeet Bhatia",
      "Subject": "[AA] Do updates of component descriptions on Data Dictionary get communicated back to the Analytics 2.0 API?",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=8bd4f5a3-6016-ef11-9f8a-6045bd02b206"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=8bd4f5a3-6016-ef11-9f8a-6045bd02b206",
+     "tag":[]
     },
     {
      "createdOn": "5\/24\/2024",
      "publisherName": "Nilotpal Bharti",
      "Subject": "How can logs be tracked for projects that were unshared from the Company folder?",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=6aaaab72-801b-ef11-840b-6045bd034c54 "
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=6aaaab72-801b-ef11-840b-6045bd034c54 ",
+     "tag":[]
     },
     {
      "createdOn": "5\/24\/2024",
      "publisherName": "Nilotpal Bharti",
      "Subject": "How to resolve garbled values in reporting?",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=22a6bd19-841b-ef11-840b-6045bd034c54 "
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=22a6bd19-841b-ef11-840b-6045bd034c54 ",
+     "tag":['reporting']
     },
     {
      "createdOn": "5\/24\/2024",
      "publisherName": "Nilotpal Bharti",
      "Subject": "Move to' option not available for Admins",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=d364d7a3-871b-ef11-840b-6045bd034c54"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=d364d7a3-871b-ef11-840b-6045bd034c54",
+     "tag":[]
     },
     {
      "createdOn": "5\/24\/2024",
      "publisherName": "Nilotpal Bharti",
      "Subject": "How to block Analytics cookies from being set without ECID extension?",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=a5b42afb-8a1b-ef11-840b-6045bd034c54"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=a5b42afb-8a1b-ef11-840b-6045bd034c54",
+     "tag":['cookies', 'ecid']
     },
     {
      "createdOn": "             5\/31\/2024",
      "publisherName": "Mohammad Faizan",
      "Subject": "Daily data feeds not receiving Adobe Advertising Cloud data sources.",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=f2e74f34-7119-ea11-a811-000d3a5936c5&forceUCI=1&pagetype=entityrecord&etn=knowledgearticle&id=d641645e-bf1e-ef11-840b-6045bd006704"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=f2e74f34-7119-ea11-a811-000d3a5936c5&forceUCI=1&pagetype=entityrecord&etn=knowledgearticle&id=d641645e-bf1e-ef11-840b-6045bd006704",
+     "tag":['data feeds']
+    
     },
     {
      "createdOn": "    5\/31\/2024",
      "publisherName": "Mohammad Faizan",
      "Subject": "Fallout discrepancy  Visits vs Visitor fallout report showing different numbers for same touchpoints",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=8d3e9ad6-c11e-ef11-840b-6045bd006704"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=8d3e9ad6-c11e-ef11-840b-6045bd006704",
+     "tag":[]
     },
     {
      "createdOn": "             5\/31\/2024",
      "publisherName": "Mohammad Faizan",
      "Subject": "List vars not getting tracked in server calls.",
-     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=6a189a64-6c1f-ef11-840a-002248092444"
+     "articleURL": "https:\/\/adobe-ent.crm.dynamics.com\/main.aspx?appid=c8f3a4cd-a068-e911-a957-000d3a34e00b&pagetype=entityrecord&etn=knowledgearticle&id=6a189a64-6c1f-ef11-840a-002248092444",
+      "tag":['List vars']
     }
    ]
-   
+
+$(document).ready(function() {
+    $('#publisherName').chosen();
+    $('#quater').chosen();
+    drawtable(Sheet1)
+    const tagSet = new Set();          
+    // Iterate over JSON data to extract tag names
+   Sheet1.forEach(item => {
+        item.tag.forEach(tag => {
+            tagSet.add(tag);
+        });
+    });
+
+    // Populate the select element with unique tag names
+   tagSet.forEach(tag => {
+        const option = $('<option></option>').attr('value', tag).text(tag);
+        $('#tags').append(option);
+    });
+
+    // Initialize Chosen on the select element
+    $('#tags').chosen();
+    console.log('dropdown')
     
+});
+$('#filter').on('click',function(){
+        const selectedQuater = $('#quater').val();
+        const selectedPublisher =$('#publisherName').val();
+        const selectedtag =$('#tags').val();
+       var filteredData = filterData(selectedQuater,selectedPublisher,selectedtag)
+
+       if(filteredData.length>0){
+        dataTable.destroy();
+        console.log(filteredData)
+        // Redraw DataTable with new JSON data
+         drawtable(JSON.parse(filteredData))
+       }
+            
+})
+// $(document).ready(function() {
+//     const $select = $('#example');
+    
+//     // Populate the select element with options from the JSON data
+//     jsonData.forEach(item => {
+//         const option = $('<option></option>').attr('value', item.publisherName).text(item.publisherName);
+//         $select.append(option);
+//     });
+
+//     // Initialize Chosen on the select element
+//     $select.chosen();
+
+//     // Event listener to log selected options to the console
+//     $select.on('change', function() {
+//         const selectedOptions = $(this).val();
+//         console.log('Selected options:', selectedOptions);
+//     });
+// });
+
+function filterData(selectedQuater,selectedPublisher,selectedtag){
+
+    const filteredData = Sheet1.filter(item => {
+        //  const publisherMatch = selectedPublisher.length === 0 || item.publisherName.some(publisher => selectedPublishers.includes(publisher));
+        const publisherMatch = selectedPublisher.length === 0 || selectedPublisher.includes(item.publisherName);
+        // const categoryMatch = selectedPublisher.length === 0 || item.category.some(category => selectedCategories.includes(category))
+        //revisit
+        const tagMatch = selectedtag.length === 0 || item.tag.some(tag => selectedtag.includes(tag));
+
+        return publisherMatch && tagMatch;
+    });
+
+   let updatedData= JSON.stringify(filteredData, null, 2);
+   return updatedData;
+}
+
 function format(d) {
     // `d` is the original data object for the row
     return (
         '<dl>' +
-        '<dt style="display: inline-block">Description:</dt>' +
-        '<dd style="display: inline-block">' +
-        d.description +
+        '<dt style="display: inline-block; font-weight:600">Article URL:</dt>' +
+        '<dd style="display: inline-block" >' +'<a class="link">'+
+        d.articleURL +'</a>'+
         '</dd>' +
         '</dl>'
     );
 }
 
-let table = new DataTable('#myTable', {
- data:test,
- columns: [
-    {
-        className: 'dt-control',
-        orderable: false,
-        data: null,
-        defaultContent: ''
-    },
-    { data: 'name' },
-     {data: ''},
-    { data: 'createdOn' },
-    { data: 'createdBy' },  
-    { data: 'CJA' },
-    { data: 'description', visible:false },
-    { data: ''}
-   
- 
-],
-columnDefs: [
-    {
-        targets: 0,
-        render: function (data, type, row, meta) {
-            return `<span style='background-color:pink; border-radius:100%'></span>`;
-        }
-    },
-    {
-        targets: 2,
-        render: function (data, type, row, meta) {
+function drawtable(tableData){
+    dataTable=''
+     dataTable = new DataTable('#myTable', {
+        data:tableData,
+        columns: [
+           {
+               className: 'dt-control',
+               orderable: false,
+               data: null,
+               defaultContent: ''
+           },
+           { data: 'Subject' },
+            {data: ''},
+           { data: 'createdOn' },
+           { data: 'publisherName' },  
+           { data: 'CJA' },
+           { data: 'articleURL', visible:false },
+           { data: ''}
+          
+        
+       ],
+       columnDefs: [
+           {
+               targets: 0,
+               render: function (data, type, row, meta) {
+                   return `<span style='background-color:pink; border-radius:100%'></span>`;
+               }
+           },
+           {
+               targets: 2,
+               render: function (data, type, row, meta) {
+               
+                if(row.tag !=undefined){
+                    console.log(row,row.tag,row.tag.length)
+                    if(row.tag.length >0){
+                        return `<p class='tags'>${row.tag[0]}</p>`;
+                      }
+                      else{
+                        return `<p class='tags'></p>`;
+                      }
 
-            return `<p class='tags'>${row.tags[0]}</p>`;
-        }
-    },
-    {
-        targets: 7,
-        render: function (data, type, row, meta) {
+                }
+                  
+               }
+           },
+           {
+               targets: 7,
+               render: function (data, type, row, meta) {
+       
+                   return `<p class='tags'>delete</p>`;
+               }
+           },
+       
+           {
+                   "defaultContent": "-",
+                   "targets": "_all"
+                 }
+       ],
+       // columnDefs: [{
+       //     "defaultContent": "-",
+       //     "targets": "_all"
+       //   }]
+       })
+       dataTable.on('click', 'td.dt-control', function (e) {
+           let tr = e.target.closest('tr');
+           let row = dataTable.row(tr);
+        
+           if (row.child.isShown()) {
+               // This row is already open - close it
+               row.child.hide();
+           }
+           else {
+               // Open this row
+               row.child(format(row.data())).show();
+               $('.link').on('click', function(){
+                 window.open($(this).text())
+               })
+           }
+       });
+}
 
-            return `<p class='tags'>delete</p>`;
-        }
-    },
-
-    {
-            "defaultContent": "-",
-            "targets": "_all"
-          }
-],
-// columnDefs: [{
-//     "defaultContent": "-",
-//     "targets": "_all"
-//   }]
-})
-table.on('click', 'td.dt-control', function (e) {
-    let tr = e.target.closest('tr');
-    let row = table.row(tr);
- 
-    if (row.child.isShown()) {
-        // This row is already open - close it
-        row.child.hide();
-    }
-    else {
-        // Open this row
-        row.child(format(row.data())).show();
-    }
-});
 
 
 $('#toggleToolbar').on('click', function(){
